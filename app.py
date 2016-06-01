ef90e2819e23d0d507793167a29b3656cd5cdbc7
Nathan Roberson <nathaniroberson@gmail.com>
2016-05-31 23:48:38 -0700

    adding passwd protection

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+from scipy.sparse import hstack
+from flask.ext.basicauth import BasicAuth
 
 DEBUG = True
 SECRET_KEY = 'development key'
@@ -6,14 +8,19 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'vt'
+app.config['BASIC_AUTH_PASSWORD'] = 'hokies'
 
+basic_auth = BasicAuth(app)
 
 @app.route('/')
+@basic_auth.required
 def home():
     return render_template('query.html')
 
 
 @app.route('/query', methods=['POST', 'GET'])
+@basic_auth.required
 def submit_query():
     title = None
     abstract = None
@@ -35,23 +42,22 @@ def submit_query():
         except KeyError:
             return render_template('query.html', error=KeyError)
 
-        # feature_model_title = pickle.load(download('title_feature_model.dill'))
-        # title_vector = feature_model_title.transform([title])
+        feature_model_title = pickle.load(download('title_feature_model.dill'))
+        title_vector = feature_model_title.transform([title])
 
-        # feature_model_abstract = pickle.load(download('abstract_feature_model.dill'))
-        # abstract_vector = feature_model_abstract.transform([abstract])
+        feature_model_abstract = pickle.load(download('abstract_feature_model.dill'))
+        abstract_vector = feature_model_abstract.transform([abstract])
 
-        # feature_model_claims = pickle.load(download('claims_feature_model.dill'))
-        # claims_vector = feature_model_claims.transform([claims])
+        feature_model_claims = pickle.load(download('claims_feature_model.dill'))
+        claims_vector = feature_model_claims.transform([claims])
 
-        # feature_vector = hstack([title_vector, abstract_vector])
-        # feature_vector = hstack([feature_vector, claims_vector])
+        feature_vector = hstack([title_vector, abstract_vector])
+        feature_vector = hstack([feature_vector, claims_vector])
 
-        # classifier = pickle.load(download('SGD2016-05-03'))
-        #
-        # group = classifier.predict(feature_vector)
-        # return render_template('query.html', group=group)
-        return
+        classifier = pickle.load(download('SGD2016-05-03'))
+
+        group = classifier.predict(feature_vector)
+        return render_template('query.html', group=group)
 
 if __name__ == '__main__':
     from os import environ
