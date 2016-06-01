fb2c66e6ebb41dfc5d3761c8cca2a2f4db2e4926
Nathan Roberson <nathaniroberson@gmail.com>
2016-05-31 21:33:26 -0700

    Updating with app

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, request, render_template
-from factory import Factory
-from config import Config
+from flask import Flask, request, render_template, redirect, url_for
+import os, json, boto
+#import sys
+#from datetime import date
+from scipy.sparse import hstack
+from dill import pickle
+import pickle
+import os
+from datetime import date
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+import tempfile
+
 
 
 DEBUG = True
@@ -9,6 +19,14 @@ USERNAME = 'admin'
 PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+conn = S3Connection()
+#AWS_ACCESS_KEY_ID = AKIAJPYNQBFLNNVKU3UQ
+#AWS_SECRET_ACCESS_KEY = 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
+mybucket = conn.get_bucket('patent-model-data')
+
+
+
 
 
 @app.route('/')
@@ -38,12 +56,38 @@ def submit_query():
         except KeyError:
             return render_template('query.html', error=KeyError)
 
-        config = Config()
-        f = Factory(config)
-        feature_vector = f.evaluate(title, abstract, claims)
-        group = f.predict(feature_vector)
+
+        def download(file):
+            key = Key(mybucket, file)
+            tempfilename = tempfile.mktemp()
+            key.get_file(open(tempfilename, "w"))
+            return open(tempfilename,'rb')
+
+
+
+        feature_model_title = pickle.load(download('title_feature_model.dill'))
+        title_vector = feature_model_title.transform([title])
+
+        feature_model_abstract = pickle.load(download('abstract_feature_model.dill'))
+        abstract_vector = feature_model_abstract.transform([abstract])
+
+        feature_model_claims = pickle.load(download('claims_feature_model.dill'))
+        claims_vector = feature_model_claims.transform([claims])
+
+        feature_vector = hstack([title_vector, abstract_vector])
+        feature_vector = hstack([feature_vector, claims_vector])
+
+        #path = config.get_classifier_path('SGD2016-05-03', False)
+        classifier = pickle.load(download('SGD2016-05-03'))
+
+
+        group = classifier.predict(feature_vector)
+
         return render_template('query.html', group=group)
 
+
+
 if __name__ == '__main__':
-    config = Config(False)
-    app.run(host=config.host)
+    from os import environ
+    app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
+    #app.run()
