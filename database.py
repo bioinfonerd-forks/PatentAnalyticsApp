6b0a2a9012b2e89b37d1f4ccd7d74ba0f0e729fc
Nathan Roberson <nathaniroberson@gmail.com>
2016-05-31 22:28:21 -0700

    Implementing mongodb

--- database.py
+++ database.py
@@ -3,6 +3,18 @@ from bson.json_util import dumps
 from config import Config
 from sklearn.feature_extraction.text import TfidfVectorizer
 from analyzer import Analyzer
+from pymongo import MongoClient
+
+class Database(object):
+    def __init__(self, config):
+        self.config = config
+        self.client=self.connect()
+
+    def connect(self):
+        client = MongoClient(self.config.db_uri)
+        client.the_database.authenticate(self.config.user_name, self.config.passwd, mechanism='SCRAM-SHA-1')
+        return client
+
 
 def translate_to_bson(dill_object):
     obj = pickle.load(dill_object)
