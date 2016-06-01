a1f3b8733d9acccd99c1c2b024342d155978c4d9
Nathan Roberson <nathaniroberson@gmail.com>
2016-05-31 23:02:54 -0700

    fixing procfile updating connection

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
-from datetime import date
 
 
 class Config(object):
     def __init__(self, debug=True):
         self.base_dir = os.path.dirname(os.path.abspath(__file__))
-        self.database_uri='ds021333.mlab.com:21333/heroku_bx11p419'
-        self.user_name='mongo_test_user'
-        self.passwd='testmong0'
+        self.MONGODB_URI = 'mongodb://mongo_test_user:testtest0@ds021333.mlab.com:21333/heroku_bx11p419'
--- database.py
+++ database.py
@@ -4,37 +4,49 @@ from config import Config
 from analyzer import Analyzer
 from pymongo import MongoClient
 
-
 class Database(object):
     def __init__(self, config):
         self.config = config
         self.client=self.connect()
 
     def connect(self):
-        client = MongoClient(self.config.db_uri)
+        parsed = urlsplit(url)
+        db_name = parsed.path[1:]
+
+        # Get your DB
+        db = Connection(url)[db_name]
+
+        # Authenticate
+        if '@' in url:
+            user, password = parsed.netloc.split('@')[0].split(':')
+            db.authenticate(user, password)
+        client = MongoClient(self.config.database_uri)
         client.the_database.authenticate(self.config.user_name, self.config.passwd, mechanism='SCRAM-SHA-1')
         return client
 
+    def serialize(self, analyzer_object):
+        obj = pickle.load(analyzer_object)
+        vocab = obj.vocabulary_
+        vocab = {(k, str(v)) for k, v in vocab.items()}
+        bson_object = dumps([vocab])
+        return bson_object
 
-def serialize(analyzer_object):
-    obj = pickle.load(analyzer_object)
-    vocab = obj.vocabulary_
-    vocab = {(k,str(v)) for k,v in vocab.items()}
-    bson_object = dumps([vocab])
-    return bson_object
-
-
-def unserialize(bson_object):
-    vocab=loads(bson_object)
-    model = Analyzer.initialize_model(3, vocab=vocab)
+    def unserialize(self, bson_object):
+        vocab = loads(bson_object)
+        vocab = {(k, int(v)) for k, v in vocab.items()}
+        model = Analyzer.initialize_model(3, vocab=vocab)
+        return model
 
 
 if __name__ == "__main__":
     config = Config()
+    database = Database(config)
     models = ["title_feature_model.dill", "abstract_feature_model.dill", "claims_feature_model.dill"]
     path = """D:\\Workspace\\PatentAnalyticsApp\\models\\""" + models[0]
-    model_bson = serialize(open(path, 'rb'))
-    model = unserialize(model_bson)
+    model_bson = database.serialize(open(path, 'rb'))
+    database.push(model_bson)
+    database.pull(model_id)
+    model = database.unserialize(model_bson)
     print(model)
 
 
