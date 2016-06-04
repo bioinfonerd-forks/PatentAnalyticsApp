c6d053c0db575663b1239ea7f9612aa8c907102c
William D Ermlick <ermlickw@vt.edu>
2016-06-04 03:41:50 -0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -3,7 +3,7 @@ import tempfile
 from boto.s3.key import Key
  
 def download(file):
-     key = Key(mybucket, file)
+     key = Key(S3Connection().get_bucket('patent-model-data'), file)
      tempfilename = tempfile.mktemp()
      key.get_contents_to_filename(tempfilename)
      return open(tempfilename,'rb')
