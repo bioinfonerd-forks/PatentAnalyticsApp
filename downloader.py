7a0eda0fd6a75c58b8deea27c2e2517eb8b1d6db
William D Ermlick <ermlickw@vt.edu>
2016-06-04 03:34:46 -0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,9 +1,9 @@
- import requests 
- import tempfile
- from boto.s3.key import Key
+import requests 
+import tempfile
+from boto.s3.key import Key
  
-  def download(file):
-            key = Key(mybucket, file)
-            tempfilename = tempfile.mktemp()
-            key.get_contents_to_filename(tempfilename)
-            return open(tempfilename,'rb')
+def download(file):
+     key = Key(mybucket, file)
+     tempfilename = tempfile.mktemp()
+     key.get_contents_to_filename(tempfilename)
+     return open(tempfilename,'rb')
