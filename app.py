142e5486588cbfff18ee65367c39794cbc98bf4c
William D Ermlick <ermlickw@vt.edu>
2016-06-05 01:53:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -66,22 +66,6 @@ def submit_query():
             return render_template('query.html', error=KeyError)
 
         
-        config = Config()
-        database = Database(config)
-        feature_model_title = pickle.load(download('title_feature_model.dill'))
-        title_vector = feature_model_title.transform([title])
-
-        feature_model_abstract = pickle.load(download('abstract_feature_model.dill'))
-        abstract_vector = feature_model_abstract.transform([abstract])
-
-        feature_model_claims = pickle.load(download('claims_feature_model.dill'))
-        claims_vector = feature_model_claims.transform([claims])
-
-        feature_vector = hstack([title_vector, abstract_vector])
-        feature_vector = hstack([feature_vector, claims_vector])
-        
-        classifier = database.pull_classifier()
-        group = classifier.predict(feature_vector)
         
         #path = config.get_classifier_path('SGD2016-05-03', False)
         #classifier = pickle.load(download('SGD2016-05-03'))
