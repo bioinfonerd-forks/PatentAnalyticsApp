2841fa77cf6ab46208b123801078358c8c345777
William D Ermlick <ermlickw@vt.edu>
2016-06-05 02:16:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -67,13 +67,34 @@ def submit_query():
         except KeyError:
             return render_template('query.html', error=KeyError)
 
+    def download(file):
+        key = Key(S3Connection().get_bucket('patent-model-data'), file)
+        tempfilename = tempfile.mktemp()
+        key.get_contents_to_filename(tempfilename)
+        return open(tempfilename,'rb')
+
+    def doitall(title, abstract, claims):
+        config = Config()
+        database = Database(config)
+        feature_model_title = pickle.load(download('title_feature_model.dill'))
+        title_vector = feature_model_title.transform([title])
+
+        feature_model_abstract = pickle.load(download('abstract_feature_model.dill'))
+        abstract_vector = feature_model_abstract.transform([abstract])
+
+        feature_model_claims = pickle.load(download('claims_feature_model.dill'))
+        claims_vector = feature_model_claims.transform([claims])
+
+        feature_vector = hstack([title_vector, abstract_vector])
+        feature_vector = hstack([feature_vector, claims_vector])
+        
+        classifier = database.pull_classifier()
+        return classifier.predict(feature_vector)
         
         
-        #path = config.get_classifier_path('SGD2016-05-03', False)
-        #classifier = pickle.load(download('SGD2016-05-03'))
-        #group = classifier.predict(feature_vector)
         group = None 
-        group = q.enqueue(doitall(title, abstract, claims))
+        #group = q.enqueue(doitall(title, abstract, claims))
+        group= doitall(title,abstract,claims)
         
         while type(group) == 'Job': 
             time.sleep(5) 
