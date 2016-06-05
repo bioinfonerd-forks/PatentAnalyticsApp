1247fab58050ec0d2df7422d7839f492149268d7
William D Ermlick <ermlickw@vt.edu>
2016-06-05 01:55:18 -0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -8,3 +8,21 @@ def download(file):
      tempfilename = tempfile.mktemp()
      key.get_contents_to_filename(tempfilename)
      return open(tempfilename,'rb')
+
+def doitall():
+      config = Config()
+      database = Database(config)
+      feature_model_title = pickle.load(download('title_feature_model.dill'))
+      title_vector = feature_model_title.transform([title])
+
+      feature_model_abstract = pickle.load(download('abstract_feature_model.dill'))
+      abstract_vector = feature_model_abstract.transform([abstract])
+
+      feature_model_claims = pickle.load(download('claims_feature_model.dill'))
+      claims_vector = feature_model_claims.transform([claims])
+
+      feature_vector = hstack([title_vector, abstract_vector])
+      feature_vector = hstack([feature_vector, claims_vector])
+        
+      classifier = database.pull_classifier()
+      return group = classifier.predict(feature_vector)
