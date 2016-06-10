aee3718eb3118b2bec8fc97274d0e9210ab56139
William D Ermlick <ermlickw@vt.edu>
2016-06-10 13:54:43 -0400

    Update database.py

--- database.py
+++ database.py
@@ -76,6 +76,7 @@ class Database(object):
 
     def pull_classifier(self, classifier_name):
         # classifier_name = "Perceptron2016-06-06"
+        classifier_name = "SGD21242636"
         db_model = self.get('classifiers', classifier_name)
         return self.deserialize(db_model['model'])
 
